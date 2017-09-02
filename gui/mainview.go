c2a19bee1025c122274d3712d455ac1c25df15d1
HubiDev <lukas.huber7@gmail.com>
2017-09-02 12:45:50 +0200

    Started with MainView

--- gui/mainview.go
+++ gui/mainview.go
@@ -1,6 +1,16 @@
 package gui
 
 type MainView struct {
+	Cur *Cursor
+}
+
+// NewMainView created a new instance of the MainView struct
+func NewMainView() (res *MainView) {
+
+	res = new(MainView)
+	res.Cur = InitCursor()
+
+	return
 }
 
 func Run() {
